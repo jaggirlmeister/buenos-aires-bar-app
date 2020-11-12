5c50069266b052606331e2350951e1c41ede35d4
Martina <mfranco0305@gmail.com>
2020-11-12 17:09:33 -0300

    delete home

--- containers/index.js
+++ containers/index.js
@@ -2,6 +2,3 @@
 export { default as Layout } from './Layout';
 export { default as Main } from './Main';
 export { default as Home } from './Home';
-export { default as Login } from './Login';
-export { default as SignUp } from './SignUp';
-export { default as Dashboard } from './Dashboard';
